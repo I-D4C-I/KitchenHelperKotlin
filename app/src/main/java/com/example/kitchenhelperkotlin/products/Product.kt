e1b055a0030f020c2eab8c0c0a33c9975b662148
Anton <anton_davydenko@mail.ru>
2023-02-05 19:01:51 +0700

    Measure. Added a unit of measurement in product

--- app/src/main/java/com/example/kitchenhelperkotlin/products/Product.kt
+++ app/src/main/java/com/example/kitchenhelperkotlin/products/Product.kt
@@ -3,9 +3,9 @@ package com.example.kitchenhelperkotlin.products
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.kitchenhelperkotlin.products.UnitOfMeasure.Measure
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
-
 
 @Parcelize
 @Entity(tableName = "product_table")
@@ -13,5 +13,6 @@ data class Product(
     val title: String,
     val amount: Int,
     val date: LocalDate = LocalDate.now(),
+    val measure: Measure = Measure.kg,
     @PrimaryKey(autoGenerate = true) val id: Int = 0
 ) : Parcelable
---
+++ app/src/main/java/com/example/kitchenhelperkotlin/products/UnitOfMeasure/Measure.kt
@@ -0,0 +1,16 @@
+package com.example.kitchenhelperkotlin.products.UnitOfMeasure
+
+enum class Measure(val unit: String) {
+
+    /*
+    TODO: Узнать как использовать строки из resources.
+     Resources.getSystem().getString(R.string.kg) не работает
+     */
+
+    kg("Кг"),
+    g("г"),
+    l("Л"),
+    ml("мЛ"),
+    stuff("Шт"),
+    pack("Уп")
+}
