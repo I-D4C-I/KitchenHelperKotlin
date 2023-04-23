4cf8a846be6f05a7d70ea220e690375ec6750606
Anton <anton_davydenko@mail.ru>
2023-04-23 14:23:08 +0700

    Product. Fix sorting by time bug.

--- app/src/main/java/com/example/kitchenhelperkotlin/util/Converters.kt
+++ app/src/main/java/com/example/kitchenhelperkotlin/util/Converters.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class Converters {
-    private val formatter = DateTimeFormatter.ofPattern("dd-MMM-yy")
+    private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
     @TypeConverter
     fun fromString(value: String?): LocalDate? {
