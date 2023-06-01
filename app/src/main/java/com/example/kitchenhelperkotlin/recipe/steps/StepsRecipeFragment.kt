8ae1a88145302a698e0d65c1adb1f4760b81a47d
Anton <anton_davydenko@mail.ru>
2023-06-01 12:06:49 +0700

    Steps. Attached viewModel to Fragment

--- app/src/main/java/com/example/kitchenhelperkotlin/recipe/steps/StepsRecipeFragment.kt
+++ app/src/main/java/com/example/kitchenhelperkotlin/recipe/steps/StepsRecipeFragment.kt
@@ -3,14 +3,34 @@ package com.example.kitchenhelperkotlin.recipe.steps
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.kitchenhelperkotlin.R
 import com.example.kitchenhelperkotlin.databinding.FragmentStepsRecipeBinding
+import javax.inject.Inject
 
 class StepsRecipeFragment : Fragment(R.layout.fragment_steps_recipe) {
+
+    @Inject
+    lateinit var factory: StepsRecipeViewModel.ViewFactory
+
+    private val viewModel: StepsRecipeViewModel by viewModels {
+        StepsRecipeViewModel.provideFactory(factory, this, arguments)
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentStepsRecipeBinding.bind(view)
 
+        fun refreshPart() {
+            binding.eRecipeStep.clearFocus()
+            binding.vPart.text = getString(R.string.part, viewModel.numberOfStep + 1)
+            binding.eRecipeStep.setText(viewModel.step)
+        }
+
+        binding.apply {
+            refreshPart()
+
+        }
     }
 }
--- app/src/main/java/com/example/kitchenhelperkotlin/recipe/steps/StepsRecipeViewModel.kt
+++ app/src/main/java/com/example/kitchenhelperkotlin/recipe/steps/StepsRecipeViewModel.kt
@@ -1,4 +1,52 @@
 package com.example.kitchenhelperkotlin.recipe.steps
 
-class StepsRecipeViewModel {
+import android.app.Application
+import android.os.Bundle
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+import com.example.kitchenhelperkotlin.R
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+
+class StepsRecipeViewModel @AssistedInject constructor(
+    application: Application,
+    @Assisted private val state: SavedStateHandle
+) : AndroidViewModel(application) {
+
+    var numberOfStep = 0
+
+    private val stepsRecipe = state.get<ArrayList<String>>("steps") ?: arrayListOf(
+        getApplication<Application>().resources.getString(
+            R.string.errorDescription)
+    )
+    var step = stepsRecipe[numberOfStep]
+
+
+
+    @AssistedFactory
+    interface ViewFactory {
+        fun create(handle: SavedStateHandle): StepsRecipeViewModel
+    }
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: ViewFactory,
+            owner: SavedStateRegistryOwner,
+            defaultArgs: Bundle? = null,
+        ): AbstractSavedStateViewModelFactory =
+            object : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+                @Suppress("UNCHECKED_CAST")
+                override fun <T : ViewModel> create(
+                    key: String,
+                    modelClass: Class<T>,
+                    handle: SavedStateHandle
+                ): T {
+                    return assistedFactory.create(handle) as T
+                }
+            }
+    }
 }
