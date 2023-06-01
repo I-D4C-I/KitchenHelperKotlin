c8a8d152767f5dd5338423f8a99936746b7bf10d
Anton <anton_davydenko@mail.ru>
2023-06-01 11:24:41 +0700

    Steps. Create layout, attached nav graph

--- app/src/main/java/com/example/kitchenhelperkotlin/recipe/steps/StepsRecipeFragment.kt
+++ app/src/main/java/com/example/kitchenhelperkotlin/recipe/steps/StepsRecipeFragment.kt
@@ -1,4 +1,16 @@
 package com.example.kitchenhelperkotlin.recipe.steps
 
-class StepsRecipeFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.kitchenhelperkotlin.R
+import com.example.kitchenhelperkotlin.databinding.FragmentStepsRecipeBinding
+
+class StepsRecipeFragment : Fragment(R.layout.fragment_steps_recipe) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentStepsRecipeBinding.bind(view)
+
+    }
 }
